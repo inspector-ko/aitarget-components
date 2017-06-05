0ff6067305e0f964867d821e36af3c545a257bf9
inspector-ko <cossoff@yandex.ru>
2017-06-05 20:21:53 +0300

    add route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -10,5 +10,6 @@ export const routes: Routes = [
   {path: 'geo', loadChildren: './demo-geo/index#DemoGeoModule'},
   {path: 'detailed-targeting', loadChildren: './demo-detailed-targeting/index#DemoDetailedTargetingModule'},
   {path: 'targeting', loadChildren: './demo-targeting/index#DemoTargetingModule'},
+  {path: 'gallery', loadChildren: './demo-gallery/index#DemoGeoGalleryModule'},
   {path: '**', component: NotFound404Component}
 ];
